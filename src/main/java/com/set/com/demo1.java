f765ee028ba7f2eb9ecf2b14bd76246f4ab595f4
wulinzhu <18515831680@163.com>
2022-07-15 17:01:56 +0800

    HashMap

--- src/main/java/com/set/com/demo1.java
+++ src/main/java/com/set/com/demo1.java
@@ -1,9 +1,15 @@
 package com.set.com;
 
+import java.util.HashMap;
+
 public class demo1 {
 
     public static void main(String[] args) {
-        System.out.println(111);
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(1, "aaa");
+        hashMap.put(2, "bbb");
+        hashMap.put(3, "ccc");
+        System.out.println(hashMap);
     }
 
 }
